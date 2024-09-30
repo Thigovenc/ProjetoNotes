388603aa65e2c34cbcf7a9294feed970a1fc38df
thigovenc <thiagovenceslau1997@gmail.com>
2024-09-30 14:12:58 -0300

    feat: project structure and global styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+
+import {ThemeProvider} from 'styled-components'
+import GlobalStyles from './styles/global'
+import theme from './styles/theme'
+import {Details} from './pages/Details/index'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
+  <StrictMode >
+    <ThemeProvider theme={theme}>
+      <Details/>
+      <GlobalStyles/>
+    </ThemeProvider>
   </StrictMode>,
 )
