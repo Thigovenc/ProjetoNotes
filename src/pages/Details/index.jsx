ef91c21935bb8645bd50a0052a71830bdd12e05e
thigovenc <thiagovenceslau1997@gmail.com>
2024-10-01 13:39:29 -0300

    feat: add the component Header

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,12 +1,12 @@
 import {Container} from './styles'
-import {Button} from '../../components/button'
+import {Button} from '../../components/Button'
+import { Header } from '../../components/Header'
 
 export function Details(){
   return(
     <Container>
-      <h1>Hello World !!</h1>
-      <span>Thiago</span>
-      <Button title="entrar" loading/>
+      <Header/>
+      <Button title="voltar"/>
     </Container>
   )
 }
