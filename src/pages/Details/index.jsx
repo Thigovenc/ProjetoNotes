8e939c748cc01f56448bb0f92c6713a84924b549
thigovenc <thiagovenceslau1997@gmail.com>
2024-10-02 09:45:24 -0300

    feat: add component section and children concept

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,11 +1,22 @@
-import {Container} from './styles'
+import {Container,Links} from './styles'
 import {Button} from '../../components/Button'
 import { Header } from '../../components/Header'
+import { Section } from '../../components/Section'
 
 export function Details(){
   return(
     <Container>
       <Header/>
+       <Section title="Links Uteis"> {/*O children vai pegar tudo que esta aqui dentro  */}
+        <Links>
+          <li>
+            <a href="https://react.dev/">https://react.dev/</a>
+          </li>
+          <li>
+            <a href="https://react.dev/">https://react.dev/</a>
+          </li>
+        </Links>
+      </Section>
       <Button title="voltar"/>
     </Container>
   )
