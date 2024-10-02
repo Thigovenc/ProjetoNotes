d953da97cb595dcb05691348dcd7424c929b7641
thigovenc <thiagovenceslau1997@gmail.com>
2024-10-02 10:38:46 -0300

    feat: add more details to the details components

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,4 +1,4 @@
-import {Container,Links} from './styles'
+import {Container,Links,Content} from './styles'
 import {Button} from '../../components/Button'
 import { Header } from '../../components/Header'
 import { Section } from '../../components/Section'
@@ -9,24 +9,33 @@ export function Details(){
   return(
     <Container>
       <Header/>
-      <ButtonText title="Excluir Nota"/>
 
-      <Section title="Links Uteis"> {/*O children vai pegar tudo que esta aqui dentro  */}
-        <Links>
-          <li>
-            <a href="https://react.dev/">https://react.dev/</a>
-          </li>
-          <li>
-            <a href="https://react.dev/">https://react.dev/</a>
-          </li>
-        </Links>
-      </Section>
+      <main>
+        <Content>
+          <ButtonText title="Excluir Nota"/>
+          <h1>Introducao a React</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Id reprehenderit voluptatibus impedit accusantium eos maxime 
+            ut blanditiis alias aperiam animi! Numquam ullam perferendis 
+            libero beatae explicabo doloribus ipsam corporis praesentium.</p>
+          <Section title="Links Uteis"> {/*O children vai pegar tudo que esta aqui dentro  */}
+            <Links>
+              <li>
+                <a href="https://react.dev/">https://react.dev/</a>
+              </li>
+              <li>
+                <a href="https://react.dev/">https://react.dev/</a>
+              </li>
+            </Links>
+          </Section>
 
-      <Section title="Marcadores">
-        <Tag title="NodeJS"/>
-        <Tag title="ReactJS"/>
-      </Section>
-      <Button title="voltar"/>
+          <Section title="Marcadores">
+            <Tag title="NodeJS"/>
+            <Tag title="ReactJS"/>
+          </Section>
+          <Button title="voltar"/>
+        </Content>
+      </main>
     </Container>
   )
 }
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -11,7 +11,11 @@ export const Container = styled.div`
     width:100%;
     height: 100vh;
 
-
+    >main{
+        grid-area:content;
+        overflow-y:scroll ;
+        padding: 64px 0;
+    }
 `;
 
 export const Links = styled.ul`
@@ -25,4 +29,27 @@ export const Links = styled.ul`
         }
     }
 
+`;
+
+export const Content = styled.div`
+    max-width: 550px;
+    margin: 0 auto;
+
+    display: flex;
+    flex-direction: column;
+
+    >button:first-child{
+        align-self: end;
+    }
+
+    >h1{
+        font-size: 36px;
+        font-weight: 500;
+        padding: 64px;
+    }
+    >p{
+        font-size: 16px;
+        margin-top: 16px;
+        text-align: justify;
+    }
 `;
