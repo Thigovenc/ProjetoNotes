6b27fb8792f36e23111150e0f1f2aff808755be0
thigovenc <thiagovenceslau1997@gmail.com>
2024-10-02 10:17:25 -0300

    feat: add ButtonText component

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -2,13 +2,16 @@ import {Container,Links} from './styles'
 import {Button} from '../../components/Button'
 import { Header } from '../../components/Header'
 import { Section } from '../../components/Section'
+import { ButtonText } from '../../components/ButtonText'
 import {Tag} from '../../components/Tag'
 
 export function Details(){
   return(
     <Container>
       <Header/>
-       <Section title="Links Uteis"> {/*O children vai pegar tudo que esta aqui dentro  */}
+      <ButtonText title="Excluir Nota"/>
+
+      <Section title="Links Uteis"> {/*O children vai pegar tudo que esta aqui dentro  */}
         <Links>
           <li>
             <a href="https://react.dev/">https://react.dev/</a>
