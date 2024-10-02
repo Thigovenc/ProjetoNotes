25b8518dd938ee3c556329f403b3fd430c9a5198
thigovenc <thiagovenceslau1997@gmail.com>
2024-10-02 13:55:03 -0300

    feat: adds button criar Nota

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,7 @@
 import {Container, Brand, Menu, Search,Content, NewNote} from './styles'
 import {Header} from '../../components/Header'
 import {ButtonText} from '../../components/ButtonText'
+import { FiPlus } from 'react-icons/fi'
 
 export function Home(){
     return(
@@ -18,10 +19,11 @@ export function Home(){
 
             </Search>
             <Content>
-
+                
             </Content>
             <NewNote>
-
+                <FiPlus/>
+                Criar Nota
             </NewNote>
         </Container>
     )
