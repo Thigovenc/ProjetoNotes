e59d7957227238294fdbfb8182d7269e9588677d
thigovenc <thiagovenceslau1997@gmail.com>
2024-10-02 08:40:52 -0300

    feat: add user image

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,17 @@
-import { Container } from "./styles";
+import { Container,Profile } from "./styles";
 
 
 export function Header(){
     return(
         <Container>
+            <Profile>
+                <img src="https://github.com/thigovenc.png" alt="Foto do usuario" />
+
+                <div>
+                    <span>Bem-Vindo</span>
+                    <strong>Thiago Venceslau</strong>
+                </div>
+            </Profile>
         </Container>
     )
 }
