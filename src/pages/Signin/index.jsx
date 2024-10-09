35c3e6678303d86242a76097f54da7973665a388
thigovenc <thiagovenceslau1997@gmail.com>
2024-10-09 09:08:07 -0300

    feat: adds Page SignIn.

--- src/pages/Signin/index.jsx
+++ src/pages/Signin/index.jsx
@@ -1,10 +1,34 @@
-import {Container} from './style'
+import {Container, Form} from './style'
+import {Input} from "../../components/Input"
+import {Button} from "../../components/Button"
+import {FiLogIn,FiMail,FiLock} from 'react-icons/fi'
 
 export function Signin(){
     return(
-
         <Container>
-            
+            <Form>
+                <h1>Notes</h1>
+                <p>Aplicacao para salvar e gerenciar seus links uteis</p>
+
+                <h2>Faca seu login</h2>
+
+                <Input
+                    placeholder="E-mail"
+                    type = "text"
+                    icon = {FiMail}
+                />
+                  <Input
+                    placeholder="Senha"
+                    type = "password"
+                    icon = {FiLock}
+                />
+
+               <Button title="Entrar"/>
+                
+                <a href="#">
+                    Criar Conta
+                </a>
+            </Form>
         </Container>
     )
 }
