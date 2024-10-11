f948a0eb7487649a8ca0f8f2ae1b68df5949a0b0
thigovenc <thiagovenceslau1997@gmail.com>
2024-10-11 10:39:50 -0300

    feat: adds path routes

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,13 @@ import { createRoot } from 'react-dom/client'
 import {ThemeProvider} from 'styled-components'
 import GlobalStyles from './styles/global'
 import theme from './styles/theme'
-import {Home} from './pages/Home'
-import {New} from './pages/New'
+
+import {Routes} from './routes'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode >
     <ThemeProvider theme={theme}>
-      <New/>
+      <Routes/>
       <GlobalStyles/>
     </ThemeProvider>
   </StrictMode>,
