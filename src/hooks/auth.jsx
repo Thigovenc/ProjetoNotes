846cce83b9829d05c819332ed8a24df142a35e67
thigovenc <thiagovenceslau1997@gmail.com>
2024-10-16 13:18:43 -0300

    fix: modify axios store tokens

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -4,20 +4,18 @@ import { api } from "../services/api";
 export const AuthContext = createContext({});
 
 function AuthProvider({ children }) {
-
   const [data, setData] = useState({});
 
   async function signIn({ email, password }) {
     try {
-        const response = await api.post("/sessions", { email, password });
-        const { user, token } = response.data;
+      const response = await api.post("/sessions", { email, password });
+      const { user, token } = response.data;
 
-        localStorage.setItem("@notes:user", JSON.stringify(user));
-        localStorage.setItem("@notes:token", token);
+      localStorage.setItem("@notes:user", JSON.stringify(user));
+      localStorage.setItem("@notes:token", token);
 
-        api.defaults.headers.authorization = `Bearer ${token}`;
-        setData({ user, token });
-
+      api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+      setData({ user, token });
     } catch (error) {
       if (error.response) {
         alert(error.response.data.message);
@@ -27,29 +25,31 @@ function AuthProvider({ children }) {
     }
   }
 
-  function signOut(){
+  function signOut() {
     localStorage.removeItem("@notes:token");
     localStorage.removeItem("@notes:user");
 
     setData({});
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     const token = localStorage.getItem("@notes:token");
     const user = localStorage.getItem("@notes:user");
 
-    if (token && user){
-        api.defaults.headers.authorization = `Bearer ${token}`;
+    if (token && user) {
+      api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 
-        setData({
-            token,
-            user: JSON.parse(user)
-        })
+      setData({
+        token,
+        user: JSON.parse(user),
+      });
     }
-  },[])
+  }, []);
 
   return (
-    <AuthContext.Provider value={{ signIn,signOut,user: data.user }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ signIn, signOut, user: data.user }}>
+      {children}
+    </AuthContext.Provider>
   );
 }
 
