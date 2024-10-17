4584b58ce5424e13928c3ca40bc595ea4626fc7c
thigovenc <thiagovenceslau1997@gmail.com>
2024-10-17 11:58:48 -0300

    feat: adds selected tags

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,59 +1,81 @@
-import {Container, Brand, Menu, Search,Content, NewNote} from './styles'
-import {Header} from '../../components/Header'
-import {ButtonText} from '../../components/ButtonText'
-import { FiPlus,FiSearch } from 'react-icons/fi'
-import { Input } from '../../components/Input'
-import { Section } from '../../components/Section'
-import { Note } from '../../components/Note'
-import { useState, useEffect } from 'react'
-import { api } from '../../services/api'
-export function Home(){
+import { Container, Brand, Menu, Search, Content, NewNote } from "./styles";
+import { Header } from "../../components/Header";
+import { ButtonText } from "../../components/ButtonText";
+import { FiPlus, FiSearch } from "react-icons/fi";
+import { Input } from "../../components/Input";
+import { Section } from "../../components/Section";
+import { Note } from "../../components/Note";
+import { useState, useEffect } from "react";
+import { api } from "../../services/api";
+export function Home() {
+  const [tags, setTags] = useState([]);
+  const [tagsSelected, setTagsSelected] = useState([]);
 
-    const [tags, setTags] = useState([]);
+  function handleTagSelected(tagName) {
+    const alreadySelected = tagsSelected.includes(tagName)
 
-    
-    useEffect(() => {
-        async function fetchTags(){
-            const response = await api.get("/tags");
-            setTags(response.data);
-        }
+    if(alreadySelected){
+        const filteredTags = tagsSelected.filter(tag => tag !== tagName)
+        setTagsSelected(filteredTags);
+    }else{
+        setTagsSelected( prevState =>  [...prevState, tagName]);
+    }
+  }
 
-        fetchTags();
-    }, [])
+  useEffect(() => {
+    async function fetchTags() {
+      const response = await api.get("/tags");
+      setTags(response.data);
+    }
 
-    return(
-        <Container>
-            <Brand>
-                <h1>Notes</h1>
-            </Brand>
-            <Header/>
-            <Menu>
-                 <li><ButtonText title="Todos" isActive/></li>
-                {
-                  
-                    tags && tags.map(tag => (
-                        <li key={String(tag.id)}><ButtonText title={tag.name}/></li>
-                    ))
+    fetchTags();
+  }, []);
 
-                }
-            </Menu>
-            <Search>
-                <Input placeholder="Pesquisar pelo titulo" icon={FiSearch}/>
-            </Search>
-            <Content>
-                <Section title="Minha Notas">
-                    <Note data={{title: 'React',
-                         tags:[
-                            {id: 1,name:'react'},
-                            {id:2, name:'NodeJS'}
-                        ]
-                         }}/>
-                </Section>
-            </Content>
-            <NewNote to="/new">
-                <FiPlus/>
-                Criar Nota
-            </NewNote>
-        </Container>
-    )
+  return (
+    <Container>
+      <Brand>
+        <h1>Notes</h1>
+      </Brand>
+      <Header />
+      <Menu>
+        <li>
+          <ButtonText
+            title="Todos"
+            onClick={() => handleTagSelected("all")}
+            isActive={tagsSelected.length === 0}
+          />
+        </li>
+        {tags &&
+          tags.map((tag) => (
+            <li key={String(tag.id)}>
+              <ButtonText
+                title={tag.name}
+                onClick={() => handleTagSelected(tag.name)}
+                isActive={tagsSelected.includes(tag.name)}
+              />
+            </li>
+          ))}
+      </Menu>
+      <Search>
+        <Input placeholder="Pesquisar pelo titulo" icon={FiSearch} />
+      </Search>
+      <Content>
+        <Section title="Minha Notas">
+          <Note
+            data={{
+              title: "React",
+              tags: [
+                { id: 1, name: "react" },
+                { id: 2, name: "NodeJS" },
+              ],
+            }}
+          />
+        </Section>
+      </Content>
+      <NewNote to="/new">
+        <FiPlus />
+        Criar Nota
+      </NewNote>
+    </Container>
+  );
 }
