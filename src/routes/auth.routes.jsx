dbd6d082e311be01e3e87bd17d95cc883642fbce
thigovenc <thiagovenceslau1997@gmail.com>
2024-10-29 11:31:48 -0300

    feat: adds handling unavailable routes

--- src/routes/auth.routes.jsx
+++ src/routes/auth.routes.jsx
@@ -1,15 +1,19 @@
-import {Routes, Route} from "react-router-dom"
+import {Routes, Route, Navigate} from "react-router-dom"
 
 import {Signin} from '../pages/Signin'
 import {SignUp} from '../pages/SignUp'
 
 
 export function AuthRoutes(){
+
+    const user = localStorage.getItem("@notes:user");
+
     return(
 
         <Routes>
             <Route path="/" element={<Signin/>}/>
             <Route path="/register" element={<SignUp/>}/> 
+            {!user && <Route path="*" element={<Navigate to="/"/>}/>}
         </Routes>
     )
 }
