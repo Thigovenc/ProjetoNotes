141b1e21a69754fea5ed35382ea33a15d1729219
thigovenc <thiagovenceslau1997@gmail.com>
2024-10-29 09:37:41 -0300

    feat: new changes

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -4,12 +4,18 @@ import { Header } from "../../components/Header";
 import { Section } from "../../components/Section";
 import { ButtonText } from "../../components/ButtonText";
 import { Tag } from "../../components/Tag";
+<<<<<<< HEAD
 import { useParams, useNavigate} from "react-router-dom";
 import { useEffect, useState } from "react";
+=======
+import { useParams, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+>>>>>>> refs/remotes/origin/main
 import { api } from "../../services/api";
 
 export function Details() {
   const [data, setData] = useState(null);
+<<<<<<< HEAD
 
   const params = useParams();
   const navigate = useNavigate();
@@ -23,16 +29,36 @@ export function Details() {
     if(confirm){
       await api.delete(`notes/${params.id}`);
       navigate("/")
+=======
+  const params = useParams();
+  const navigate = useNavigate();
+
+  function handleBack() {
+    navigate(-1);
+  }
+  async function handleRemove() {
+    const confirm = window.confirm("deseja realmente excluir a nota ?");
+    if (confirm) {
+      await api.delete(`/notes/${params.id}`);
+      navigate(-1);
+>>>>>>> refs/remotes/origin/main
     }
   }
   useEffect(() => {
     async function fetchNote() {
       const response = await api.get(`/notes/${params.id}`);
+<<<<<<< HEAD
       setData(response.data);
     }
 
     fetchNote();
   }, []);
+=======
+      setData(response);
+    }
+    fetchNote();
+  });
+>>>>>>> refs/remotes/origin/main
   return (
     <Container>
       <Header />
@@ -40,8 +66,14 @@ export function Details() {
         <main>
           <Content>
             <ButtonText title="Excluir Nota" onClick={handleRemove} />
+<<<<<<< HEAD
             <h1>{data.title}</h1>
             <p>{data.description}</p>
+=======
+            <h1>Introducao a React</h1>
+            <p>{data.descriptions}</p>
+
+>>>>>>> refs/remotes/origin/main
             {data.links && (
               <Section title="Links Uteis">
                 {" "}
@@ -49,7 +81,13 @@ export function Details() {
                 <Links>
                   {data.links.map((link) => (
                     <li key={String(link.id)}>
+<<<<<<< HEAD
                       <a href={link.url} target="_blank">{link.url}</a>
+=======
+                      <a target="_blank" href={link.url}>
+                        {link.url}
+                      </a>
+>>>>>>> refs/remotes/origin/main
                     </li>
                   ))}
                 </Links>
@@ -58,11 +96,19 @@ export function Details() {
             {data.tags && (
               <Section title="Marcadores">
                 {data.tags.map((tag) => (
+<<<<<<< HEAD
                   <Tag key={String(tag.id)} title={tag.name}/>
                 ))}
               </Section>
             )}
             <Button title="voltar" onClick= {handleBack} />
+=======
+                  <Tag title={tag.name} key={String(tag.id)} />
+                ))}
+              </Section>
+            )}
+            <Button title="voltar" onClick={handleBack} />
+>>>>>>> refs/remotes/origin/main
           </Content>
         </main>
       )}
